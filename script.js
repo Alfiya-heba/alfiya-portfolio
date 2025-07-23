82604416b1104ba1d11c1c471edf373acb756fd2
Alfiya Heba <alfiyaheba10@gmail.com>
2025-07-23 16:08:15 +0530

    changes

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const sideMenu = document.getElementById("sideMenu");
-  
-    window.openMenu = function () {
-      sideMenu.style.right = "0";
-    };
-  
-    window.closeMenu = function () {
-      sideMenu.style.right = "-16rem";
-    };
+  const sideMenu = document.getElementById("sideMenu");
+  const navBar = document.querySelector("nav");
+  const navLinks = document.querySelector("nav ul");
+
+  window.openMenu = function () {
+    sideMenu.style.right = "0";
+  };
+
+  window.closeMenu = function () {
+    sideMenu.style.right = "-16rem";
+  };
+
+  window.addEventListener('scroll', () => {
+    if (scrollY > 50) {
+      navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+      navLinks.classList.remove('bg-white','shadow-sm','bg-opacity-50')
+    } else {
+      navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+      navLinks.classList.add('bg-white','shadow-sm','bg-opacity-50')
+    }
   });
-  
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html", "./*.js"],
+    theme: {
+      extend: {
+        fontFamily: {
+          Outfit: ["Outfit", "sans-serif"],
+          Ovo: ["Ovo", "serif"],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
